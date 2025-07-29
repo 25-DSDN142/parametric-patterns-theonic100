5fa4733d7c4bc75fc120a4aee0f31cefe1c12b12
theonic100 <theo.nicholson.nz.100@gmail.com>
2025-07-29 19:30:40 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -5,8 +5,8 @@ var opa = 55
 var otri = 100
 
 // centre lines
-var alllines = 0
-
+var alllines1 = 100
+var alllines = 100
 
 //triangles small pink
 var trianglex = 75
@@ -20,15 +20,18 @@ var tpinky = 20
 var circlex = 50
 var circley = 50
 var circlesize = 50
-// triangles green
+
+// triangles bpink
+
+
  
 
 
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-//pWallpaper.output_mode(GRID_WALLPAPER);
+//  pWallpaper.output_mode(DEVELOP_GLYPH);
+pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -41,149 +44,139 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
  //background(34, 12, 237, 50); //light orange background
- background(255,255,255)
+ background(255)
+ if(circlesize>50){
+ background(252, 130, 0)}
+ if(circlesize<50){background(0, 252, 239,opa)}
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-
-
-//centre lines
-//RH side
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines,alllines+200,alllines+25);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+25,alllines+200,alllines+50);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+50,alllines+200,alllines+75);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+75,alllines+200,alllines+100);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+ 200,alllines+200,alllines+175);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+175,alllines+200,alllines+150);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+150,alllines+200,alllines+125);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+200,alllines+125,alllines+200,alllines+100);
+//centre lines without opacity filter
+//RH Side
+  fill(247,107,7);
+  strokeWeight(0)
+ if(circlesize<50){
+  stroke(0,0,0)
+  strokeWeight(2)
+  //RH side
+  line(alllines1,alllines1,alllines1+100,alllines1-50,alllines1+100,alllines1);
+line(alllines1,alllines1,alllines1+100,alllines1+50,alllines1+100,alllines1+25);
 
 //top 
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+175,alllines,alllines+200,alllines);
 
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+150,alllines,alllines+175,alllines);
+line(alllines1,alllines1,alllines1+50,alllines1-100,alllines1+25,alllines1-100);
+line(alllines1,alllines1,alllines1,alllines1-100,alllines1-25,alllines1-100);
+line(alllines1,alllines1,alllines1-50,alllines1-100,alllines1-50,alllines1-100);
 
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+150,alllines,alllines+125,alllines);
+//LH side
 
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+100,alllines,alllines+125,alllines);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+100,alllines,alllines+75,alllines);
-
-fill(247, 107, 7,opa);
-strokeWeight(0);
-triangle(100,100,alllines+50,alllines,alllines+75,alllines);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines+25,alllines,alllines+50,alllines);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines,alllines,alllines+25,alllines);
-
-// LH side
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+25,alllines,alllines);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+50,alllines,alllines+25);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+75,alllines,alllines+50);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+100,alllines,alllines+75);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+100,alllines,alllines+125);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+125,alllines,alllines+150);
-
-fill(247,107,7);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+150,alllines,alllines+175);
-
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(100,100,alllines,alllines+175,alllines,alllines+200);
+line(alllines1,alllines1,alllines1-100,alllines1-50,alllines1-100,alllines1-50);
+line(alllines1,alllines1,alllines1-100,alllines1+50,alllines1-100,alllines1+75);
 
 //bottom
-fill(247,107,7);
-strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines,alllines+200,alllines+25,alllines+200);
 
+line(alllines1,alllines1,alllines1-50,alllines1+100,alllines1-25,alllines1+100);
+line(alllines1,alllines1,alllines1,alllines1+100,alllines1+25,alllines1+100);
+line(alllines1,alllines1,alllines1+50,alllines1+100,alllines1+75,alllines1+100);
+}else{
+
+triangle(alllines1,alllines1,alllines1+100,alllines1-75,alllines1+100,alllines1-50);
+triangle(alllines1,alllines1,alllines1+100,alllines1-25,alllines1+100,alllines1);
+triangle(alllines1,alllines1,alllines1+100,alllines1+100,alllines1+100,alllines1+75);
+triangle(alllines1,alllines1,alllines1+100,alllines1+50,alllines1+100,alllines1+25);
+//top 
+triangle(alllines1,alllines1,alllines1+75,alllines1-100,alllines1+100,alllines1-100);
+triangle(alllines1,alllines1,alllines1+50,alllines1-100,alllines1+25,alllines1-100);
+triangle(alllines1,alllines1,alllines1,alllines1-100,alllines1-25,alllines1-100);
+triangle(alllines1,alllines1,alllines1-75,alllines1-100,alllines1-50,alllines1-100);
+//LH side
+triangle(alllines1,alllines1,alllines1-100,alllines1-75,alllines1-100,alllines1-100);
+triangle(alllines1,alllines1,alllines1-100,alllines1-25,alllines1-100,alllines1-50);
+triangle(alllines1,alllines1,alllines1-100,alllines1,alllines1-100,alllines1+25);
+triangle(alllines1,alllines1,alllines1-100,alllines1+50,alllines1-100,alllines1+75);
+//bottom
+triangle(alllines1,alllines1,alllines1-100,alllines1+100,alllines1-75,alllines1+100);
+triangle(alllines1,alllines1,alllines1-50,alllines1+100,alllines1-25,alllines1+100);
+triangle(alllines1,alllines1,alllines1,alllines1+100,alllines1+25,alllines1+100);
+triangle(alllines1,alllines1,alllines1+50,alllines1+100,alllines1+75,alllines1+100);}
+
+//centre lines with ocacity fillter
+//RH side
 fill(247,107,7,opa);
+if(circlesize<50){fill(252, 59, 0,opa)}
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+25,alllines+200,alllines+50,alllines+200);
+triangle(alllines,alllines,alllines+100,alllines+75,alllines+100,alllines+50);
 
-fill(247,107,7);
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+50,alllines+200,alllines+75,alllines+200);
+triangle(alllines,alllines,alllines+100,alllines-100,alllines+100,alllines-75)
 
-fill(247,107,7,opa);
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+75,alllines+200,alllines+100,alllines+200);
+triangle(alllines,alllines,alllines+100,alllines-50,alllines+100,alllines-25)
 
-fill(247,107,7);
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+100,alllines+200,alllines+125,alllines+200);
+triangle(alllines,alllines,alllines+100,alllines+25,alllines+100,alllines)
 
-fill(247,107,7,opa);
-strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+125,alllines+200,alllines+150,alllines+200);
+//top 
 
-fill(247,107,7);
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+150,alllines+200,alllines+175,alllines+200);
+triangle(alllines,alllines,alllines+50,alllines-100,alllines+75,alllines-100);
 
-fill(247,107,7,opa);
+//fill(247,107,7,opa);
 strokeWeight(0);
-triangle(alllines+100,alllines+100,alllines+175,alllines+200,alllines+200,alllines+200);
+triangle(alllines,alllines,alllines,alllines-100,alllines+25,alllines-100);
+
+//fill(247, 107, 7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-50,alllines-100,alllines-25,alllines-100);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-100,alllines-100,alllines-75,alllines-100);
+
+// LH side
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-100,alllines-50,alllines-100,alllines-75);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-100,alllines,alllines-100,alllines-25);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-100,alllines+25,alllines-100,alllines+50);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-100,alllines+75,alllines-100,alllines+100);
+
+// bottom
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-75,alllines+100,alllines-50,alllines+100);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines-25,alllines+100,alllines,alllines+100);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines+25,alllines+100,alllines+50,alllines+100);
+
+//fill(247,107,7,opa);
+strokeWeight(0);
+triangle(alllines,alllines,alllines+75,alllines+100,alllines+100,alllines+100);
 
 fill(239, 245, 66,otri);
-if (circlesize > 50){ 
-  fill(29, 7, 227)}
+if (circlesize > 60){ 
+  fill(29, 7, 227,otri)}
 
 //circle
 strokeWeight(0); 
@@ -192,13 +185,28 @@ circle(circlex,circley,circlesize);//tri1
 circle(circlex+50,circley+50,circlesize);//tri2
 circle(circlex+100,circley+100,circlesize);//tri3
 
-// triangle small pink
-fill(247, 5, 183, otri);
+
+if (trianglex > 75) {
+  stroke(29, 7, 227);
+  strokeWeight(2);      
+  line(100,0,100,200);// vert line
+  line(0,100,200,100)
+ 
+}else{fill(247, 5, 183, otri);
+  //triangle small pink 
 strokeWeight(0);
 triangle(trianglex+25,triangley+25,trianglex,triangley,trianglex+50,triangley);//
 triangle(trianglex,triangley+50,trianglex-25,triangley+25,trianglex-25,triangley+75);//
 triangle(trianglex+50,triangley+50,trianglex+75,triangley+25,trianglex+75,triangley+75);//
 triangle(trianglex+25,triangley+75,trianglex,triangley+100,trianglex+50,triangley+100);//
+}
+// triangle small pink
+// fill(247, 5, 183, otri);
+// strokeWeight(0);
+// triangle(trianglex+25,triangley+25,trianglex,triangley,trianglex+50,triangley);//
+// triangle(trianglex,triangley+50,trianglex-25,triangley+25,trianglex-25,triangley+75);//
+// triangle(trianglex+50,triangley+50,trianglex+75,triangley+25,trianglex+75,triangley+75);//
+// triangle(trianglex+25,triangley+75,trianglex,triangley+100,trianglex+50,triangley+100);//
 
 // triangle big pink
 fill(247, 5, 183, otri);
@@ -208,8 +216,6 @@ triangle(tpinkx+25,tpinky+80,tpinkx,tpinky+40,tpinkx,tpinky+120);//left
 triangle(tpinkx+135,tpinky+80,tpinkx+160,tpinky+40,tpinkx+160,tpinky+120);//right
 triangle(tpinkx+80,tpinky+135,tpinkx+40,tpinky+160,tpinkx+120,tpinky+160);//bottom
 
-// triangle big green
-//fill(5, 247, 66,otri+20);
 strokeWeight(0);
 //triangle(75,75,44,25,25,44);
 triangle(75,125,25,156,44,175);
